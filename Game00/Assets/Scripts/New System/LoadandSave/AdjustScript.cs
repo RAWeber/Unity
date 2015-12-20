a4baf7357d7a2bb4910e0a45a4a53dd7d65125f2
Robert Weber <RAWeber95@gmail.com>
2015-12-19 18:04:38 -0800

    Testing persistent save/scene changes

---
+++ Game00/Assets/Scripts/New System/LoadandSave/AdjustScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdjustScript : MonoBehaviour {
+
+	void OnGUI()
+    {
+        if(GUI.Button(new Rect(10,80,100,30), "Health up"))
+        {
+            GameControl.control.health += 10;
+        }
+        if (GUI.Button(new Rect(10, 120, 100, 30), "Health down"))
+        {
+            GameControl.control.health -= 10;
+        }
+        if (GUI.Button(new Rect(10, 160, 100, 30), "Exp up"))
+        {
+            GameControl.control.experience += 10;
+        }
+        if (GUI.Button(new Rect(10, 200, 100, 30), "Exp down"))
+        {
+            GameControl.control.experience -= 10;
+        }
+        if (GUI.Button(new Rect(10, 240, 100, 30), "Save"))
+        {
+            GameControl.control.Save();
+        }
+        if (GUI.Button(new Rect(10, 280, 100, 30), "Load"))
+        {
+            GameControl.control.Load();
+        }
+    }
+}
